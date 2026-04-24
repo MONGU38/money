cbcc5b90c67a144e6b149720743a1e80bac9efa5
MONGU38 <fbi1588@gmail.com>
2026-04-24 10:16:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,31 @@
-const CACHE_NAME = 'moneyapp-v1';
+const CACHE_NAME = 'moneyapp-' + new Date().toISOString();
 const urlsToCache = [
   '/moneyblack/',
   '/moneyblack/index.html'
 ];
-
 self.addEventListener('install', function(event) {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
-
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 self.addEventListener('fetch', function(event) {
+  if (event.request.url.includes('sw.js')) return;
   event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) return response;
