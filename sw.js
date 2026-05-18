a7088e58c16e32fb788f3c05921e1a994a2f612c
MONGU38 <fbi1588@gmail.com>
2026-05-18 20:05:09 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,15 @@
-const CACHE_NAME = 'moneyapp-' + new Date().toISOString();
-const urlsToCache = [
-  '/moneyblack/',
-  '/moneyblack/index.html'
-];
 self.addEventListener('install', function(event) {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
 });
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+      return Promise.all(cacheNames.map(function(cacheName) {
+        return caches.delete(cacheName);
+      }));
+    }).then(function() {
+      return self.clients.claim();
     })
   );
 });
-self.addEventListener('fetch', function(event) {
-  if (event.request.url.includes('sw.js')) return;
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) return response;
-      return fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', function(event) {});
